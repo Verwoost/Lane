bcbda92efcd176cc791d098408c85b5ae6ce3811
JoostV <verweij.joost@gmail.com>
2022-09-28 09:22:20 +0200

    add globalstyle

--- simple-react-carousel/vite-project/src/App.jsx
+++ simple-react-carousel/vite-project/src/App.jsx
@@ -1,33 +1,16 @@
-import JoostCarousel from './components/JoostCarousel';
-import SzimonCarousel from './components/SzimonCarousel';
-import styled from 'styled-components';
-
-
-
-const PlaceHolder = styled.div`
-width: 300px;
-height: 300px;
-font-size: 1rem;
-background-color: grey;
-font-weight: bold;
-color: white;
-
-`;
-
-
+import JoostCarousel from "./components/JoostCarousel";
+import SzimonCarousel from "./components/SzimonCarousel";
+import styled from "styled-components";
 
 function App() {
-
-
   return (
     <div className="App">
       <div>
-      <JoostCarousel></JoostCarousel>
-      <SzimonCarousel></SzimonCarousel>
+        <JoostCarousel></JoostCarousel>
+        <SzimonCarousel></SzimonCarousel>
       </div>
-       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ simple-react-carousel/vite-project/src/components/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+`;
+
+export default GlobalStyle;
